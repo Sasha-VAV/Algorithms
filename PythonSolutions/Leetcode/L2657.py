addfc3c2df5d3886c8fd91cf810e0310c3a55b4e
Sashavav <vav110011@gmail.com>
2025-06-09 12:15:29 +0300

    Add LeetCode 2657

---
+++ PythonSolutions/Leetcode/L2657.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+class Solution:
+    def findThePrefixCommonArray(self, A: List[int], B: List[int]) -> List[int]:
+        a = set()
+        b = set()
+        res = [0] * len(A)
+        for i, (x, y) in enumerate(zip(A, B)):
+            a.add(x)
+            b.add(y)
+            res[i] = len(a.intersection(b))
+        return res
