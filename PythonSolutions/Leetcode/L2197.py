4955d4b937901184fda0dddf9112131fbfd71f9d
Sashavav <vav110011@gmail.com>
2025-09-17 09:09:38 +0300

    Add LeetCode 2197

---
+++ PythonSolutions/Leetcode/L2197.py
@@ -0,0 +1,12 @@
+from typing import List
+from math import gcd, lcm
+
+
+class Solution:
+    def replaceNonCoprimes(self, nums: List[int]) -> List[int]:
+        res = []
+        for x in nums:
+            res.append(x)
+            if len(res) > 1 and gcd(res[-1], res[-2]) > 1:
+                res.append(lcm(res.pop(), res.pop()))
+        return res
