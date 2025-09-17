7443b4e94db3cf2c2d09853119949caf355c9091
Sashavav <vav110011@gmail.com>
2025-09-17 09:12:12 +0300

    Fix LeetCode 2197

--- PythonSolutions/Leetcode/L2197.py
+++ PythonSolutions/Leetcode/L2197.py
@@ -7,6 +7,6 @@ class Solution:
         res = []
         for x in nums:
             res.append(x)
-            if len(res) > 1 and gcd(res[-1], res[-2]) > 1:
+            while len(res) > 1 and gcd(res[-1], res[-2]) > 1:
                 res.append(lcm(res.pop(), res.pop()))
         return res
