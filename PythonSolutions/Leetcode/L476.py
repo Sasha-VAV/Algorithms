f06ea9510f941c181b3b4d21dc7015d73d50c4ac
Sashavav <vav110011@gmail.com>
2026-03-11 10:14:03 +0300

    Add LeetCode 476

---
+++ PythonSolutions/Leetcode/L476.py
@@ -0,0 +1,6 @@
+class Solution:
+    def findComplement(self, n: int) -> int:
+        if n == 0: return 1
+        power = math.log2(n)
+        power = int(power + 1)
+        return abs(2 ** power - n - 1)
